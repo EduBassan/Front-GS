bee2fc857c4331f66cf88fcafbf3d215b619b623
EduBassan <edubassan7@gmail.com>
2025-06-06 16:23:28 -0300

    slideshow

--- js/index.js
+++ js/index.js
@@ -7,3 +7,24 @@
     menu.classList.toggle('active');
   });
 
+let slideIndex = 0;
+showSlides();
+
+function showSlides() {
+    let i;
+    let slides = document.getElementsByClassName("slide");
+    
+    for (i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";  
+    }
+    
+    slideIndex++;
+    if (slideIndex > slides.length) {slideIndex = 1}    
+    
+    slides[slideIndex - 1].style.display = "block";  
+    setTimeout(showSlides, 4000);
+}
+
+
+
+
